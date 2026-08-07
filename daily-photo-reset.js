c939b4073e9cb291353a6281ce5c1260c4704e9b
drawingeachday-glitch <drawingeachday@gmail.com>
2026-08-07 18:09:09 +0200

    Refresh safety runtime cache key

--- daily-photo-reset.js
+++ daily-photo-reset.js
@@ -26,7 +26,7 @@
   }
 
   function loadFeatureStack(){
-    loadRuntimeFeature('runtime-safety.js','stregRuntimeSafetyRuntime','20260807-2');
+    loadRuntimeFeature('runtime-safety.js','stregRuntimeSafetyRuntime','20260807-3');
     loadRuntimeFeature('home-challenges.js','stregHomeChallengesRuntime','20260807-1');
     loadRuntimeFeature('challenge-page-redesign.js','stregChallengePageRedesignRuntime','20260807-6');
     loadRuntimeFeature('daily-challenge-single-target.js','stregDailySingleTargetRuntime','20260807-2');
